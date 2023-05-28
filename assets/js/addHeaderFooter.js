ac2647267d8ac3bbdb209eb82665867bb89f7e75
JaumeG13 <jaumegenaro02@gmail.com>
2023-05-28 12:47:03 +0200

    Header and footer change

--- assets/js/addHeaderFooter.js
+++ assets/js/addHeaderFooter.js
@@ -1,11 +1,11 @@
-document.onload = function addHeaderFooter() {
+/*document.onload = function addHeaderFooter() {
     let headerHTML = "header.html"
     let header = document.getElementById('header').innerHTML = headerHTML;
     let footer = document.getElementById('footer');
 
 
 }
-/*
+
 async function addFooter() {
     const resp = await fetch("footer.html");
     const html = await resp.text();
