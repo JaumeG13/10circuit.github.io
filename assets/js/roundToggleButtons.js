f1691f978e0f7155fac7af18c1466cb0aa0c11fd
JaumeG13 <jaumegenaro02@gmail.com>
2023-06-01 10:45:27 +0200

    Simplified JS and general fixes

--- assets/js/roundToggleButtons.js
+++ assets/js/roundToggleButtons.js
@@ -9,123 +9,69 @@ var r8 = document.getElementById('round8');
 var r9 = document.getElementById('round9');
 var r10 = document.getElementById('round10');
 
-function showRound1() {
+function makeDefault() {
     for (let i = 1; i < 11; i++) {
         document.getElementById(`round${i}`).style.display = 'none';
         document.getElementById(`button${i}`).style.backgroundColor = '#975e6a';
     }
+}
+
+function showRound1() {
+    makeDefault();
     document.getElementById('round1').style.display = 'grid';
     document.getElementById('button1').style.backgroundColor = '#c18794';
 }
 
 function showRound2() {
-    for (let i = 1; i < 11; i++) {
-        document.getElementById(`round${i}`).style.display = 'none';
-        document.getElementById(`button${i}`).style.backgroundColor = '#975e6a';
-    }
+    makeDefault();
     document.getElementById('round2').style.display = 'grid';
     document.getElementById('button2').style.backgroundColor = '#c18794';
 }
 
 function showRound3() {
-    for (let i = 1; i < 11; i++) {
-        document.getElementById(`round${i}`).style.display = 'none';
-        document.getElementById(`button${i}`).style.backgroundColor = '#975e6a';
-    }
+    makeDefault();
     document.getElementById('round3').style.display = 'grid';
     document.getElementById('button3').style.backgroundColor = '#c18794';
 }
 
 function showRound4() {
-    for (let i = 1; i < 11; i++) {
-        document.getElementById(`round${i}`).style.display = 'none';
-        document.getElementById(`button${i}`).style.backgroundColor = '#975e6a';
-    }
+    makeDefault();
     document.getElementById('round4').style.display = 'grid';
     document.getElementById('button4').style.backgroundColor = '#c18794';
 }
 
 function showRound5() {
-    for (let i = 1; i < 11; i++) {
-        document.getElementById(`round${i}`).style.display = 'none';
-        document.getElementById(`button${i}`).style.backgroundColor = '#975e6a';
-    }
+    makeDefault();
     document.getElementById('round5').style.display = 'grid';
     document.getElementById('button5').style.backgroundColor = '#c18794';
 }
 
 function showRound6() {
-    for (let i = 1; i < 11; i++) {
-        document.getElementById(`round${i}`).style.display = 'none';
-        document.getElementById(`button${i}`).style.backgroundColor = '#975e6a';
-    }
+    makeDefault();
     document.getElementById('round6').style.display = 'grid';
     document.getElementById('button6').style.backgroundColor = '#c18794';
 }
 
 function showRound7() {
-    for (let i = 1; i < 11; i++) {
-        document.getElementById(`round${i}`).style.display = 'none';
-        document.getElementById(`button${i}`).style.backgroundColor = '#975e6a';
-    }
+    makeDefault();
     document.getElementById('round7').style.display = 'grid';
     document.getElementById('button7').style.backgroundColor = '#c18794';
 }
 
 function showRound8() {
-    for (let i = 1; i < 11; i++) {
-        document.getElementById(`round${i}`).style.display = 'none';
-        document.getElementById(`button${i}`).style.backgroundColor = '#975e6a';
-    }
+    makeDefault();
     document.getElementById('round8').style.display = 'grid';
     document.getElementById('button8').style.backgroundColor = '#c18794';
 }
 
 function showRound9() {
-    for (let i = 1; i < 11; i++) {
-        document.getElementById(`round${i}`).style.display = 'none';
-        document.getElementById(`button${i}`).style.backgroundColor = '#975e6a';
-    }
+    makeDefault();
     document.getElementById('round9').style.display = 'grid';
     document.getElementById('button9').style.backgroundColor = '#c18794';
 }
 
 function showRound10() {
-    for (let i = 1; i < 11; i++) {
-        document.getElementById(`round${i}`).style.display = 'none';
-        document.getElementById(`button${i}`).style.backgroundColor = '#975e6a';
-    }
+    makeDefault();
     document.getElementById('round10').style.display = 'grid';
     document.getElementById('button10').style.backgroundColor = '#c18794';
 }
-/*
-function showRound(i) {
-    for(let x = 1; x < 11; x++) {
-        document.getElementById(`round${x}`).style.display = 'none';
-        document.getElementById(`button${x}`).style.backgroundColor = '#975e6a';
-    }
-    document.getElementById(`round${i}`).style.display = 'grid';
-    document.getElementById(`button${i}`).style.backgroundColor = '#c18794';
-}
-
-function showRound(roundNumber) {
-    for (let i = 1; i < 11; i++) {
-    document.getElementById(`round${i}`).style.display = 'none';
-    document.getElementById(`button${i}`).style.backgroundColor = '#975e6a';
-    }
-    document.getElementById(`round${roundNumber}`).style.display = 'grid';
-    document.getElementById(`button${roundNumber}`).style.backgroundColor = '#c18794';
-}
-
-
-
-
-
-function showRound() {
-    for (let i = 1; i < 11; i++) {
-        document.getElementById(`round${i}`).style.display = 'none';
-        document.getElementById(`button${i}`).style.backgroundColor = '#975e6a';
-        }
-        document.getElementById('round10').style.display = 'grid';
-        document.getElementById('button10').style.backgroundColor = '#c18794';
-}*/
