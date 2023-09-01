acb01a0a6cdf5210cda68c0a152bf4b73a97e24a
JaumeG13 <jaumegenaro02@gmail.com>
2023-09-01 12:41:29 +0200

    buttons tweak

--- assets/js/roundToggleButtons.js
+++ assets/js/roundToggleButtons.js
@@ -24,16 +24,40 @@ var round10Button = document.getElementById('round10-button');
 
 */
 
+function makeDefault() {
+    document.getElementById('s2btn').style.display = 'block';
+    document.getElementById('rnd9').style.display = 'block';
+    document.getElementById('rnd10').style.display = 'block';
+}
+
 function roundToggle() {
-    if ((document.getElementById('championship-button').checked) && (document.getElementById('season1-button').checked)) {  //if championship circuit and season 1 are selected, hide round 9 and 10
+    if ((document.getElementById('championship-button').checked) && (document.getElementById('season1-button').checked)) {  /*if championship circuit and season 1 is selected, hide round 9 and 10*/
+        makeDefault();
         document.getElementById('rnd9').style.display = 'none';
         document.getElementById('rnd10').style.display = 'none';
     }
-    if ((document.getElementById('porsche-button').checked)) {  //if porsche cup is selected, hide season 2 and hide round 9 and 10
+    if ((document.getElementById('championship-button').checked) && (document.getElementById('season2-button').checked)) {  /*if championship circuit and season 2 is selected, show round 9 and 10*/
+        makeDefault();
+        document.getElementById('rnd9').style.display = 'block';
+        document.getElementById('rnd10').style.display = 'block';
+    }
+
+    if (document.getElementById('porsche-button').checked) {    /*if porsche cup is selected, hide season 2, round 9, round 10 and check season 1*/
+        makeDefault();
         document.getElementById('s2btn').style.display = 'none';
+        document.getElementById('season1-button').checked = true;
         document.getElementById('rnd9').style.display = 'none';
         document.getElementById('rnd10').style.display = 'none';
     }
+
+    /*if championship circuit, season 1 and round 9 or round 10 are selected, and you switch to championship circuit season 1 or porsche cup it will default to round 1*/
+    if ((document.getElementById('championship-button').checked) && (document.getElementById('season1-button').checked) && ((document.getElementById('round9-button').checked) || (document.getElementById('round10-button').checked))) {
+        document.getElementById('round1-button').checked = true;
+    }
+    if ((document.getElementById('porsche-button').checked) && (document.getElementById('season1-button').checked) && ((document.getElementById('round9-button').checked) || (document.getElementById('round10-button').checked))) {
+        document.getElementById('round1-button').checked = true;
+    }
+    
 
     let roundHtml;
 
@@ -86,6 +110,12 @@ function roundToggle() {
     }
     if ((document.getElementById('championship-button').checked) && (document.getElementById('season2-button').checked) && (document.getElementById('round8-button').checked)) {
         roundHtml = "/standings/championshipcircuit/season2/round8.html";
+    }
+    if ((document.getElementById('championship-button').checked) && (document.getElementById('season2-button').checked) && (document.getElementById('round9-button').checked)) {
+        roundHtml = "/standings/championshipcircuit/season2/round9.html";
+    }
+    if ((document.getElementById('championship-button').checked) && (document.getElementById('season2-button').checked) && (document.getElementById('round10-button').checked)) {
+        roundHtml = "/standings/championshipcircuit/season2/round10.html";
     }
     //PORSCHE SEASON 1
     if ((document.getElementById('porsche-button').checked) && (document.getElementById('season1-button').checked) && (document.getElementById('round1-button').checked)) {
