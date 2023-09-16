a57c76aca94aa64a19e68e9646221748f103339d
JaumeG13 <jaumegenaro02@gmail.com>
2023-09-16 22:49:18 +0200

    circuit s2 revival

--- assets/js/roundToggleButtons.js
+++ assets/js/roundToggleButtons.js
@@ -1,38 +1,17 @@
 function makeDefault() {
-    document.getElementById('rnd9').style.display = 'block';
-    document.getElementById('rnd10').style.display = 'block';
+    document.getElementById('s2btn').style.display = 'block';
 }
 
 function roundToggleStandings() {
-    if ((document.getElementById('championship-button').checked) && (document.getElementById('season1-button').checked)) {  //if championship circuit and season 1 is selected, hide round 9 and 10/
+    if ((document.getElementById('championship-button').checked)) {  //if championship circuit is selected, show season 2
         makeDefault();
-        document.getElementById('rnd9').style.display = 'none';
-        document.getElementById('rnd10').style.display = 'none';
-    }
-    /*
-    if ((document.getElementById('championship-button').checked) && (document.getElementById('season2-button').checked)) {  //if championship circuit and season 2 is selected, show round 9 and 10
-        makeDefault();
-        document.getElementById('rnd9').style.display = 'block';
-        document.getElementById('rnd10').style.display = 'block';
     }
     
-    if (document.getElementById('porsche-button').checked) {    //if porsche cup is selected, hide season 2, round 9, round 10 and check season 1
+    if (document.getElementById('porsche-button').checked) {    //if porsche cup is selected, hide season 2 and check season 1
         makeDefault();
         document.getElementById('s2btn').style.display = 'none';
         document.getElementById('season1-button').checked = true;
-        document.getElementById('rnd9').style.display = 'none';
-        document.getElementById('rnd10').style.display = 'none';
-    }
-    */
-
-    //if championship circuit, season 1 and round 9 or round 10 are selected, and you switch to championship circuit season 1 or porsche cup it will default to round 1
-    if ((document.getElementById('championship-button').checked) && (document.getElementById('season1-button').checked) && ((document.getElementById('round9-button').checked) || (document.getElementById('round10-button').checked))) {
-        document.getElementById('round1-button').checked = true;
-    }
-    if ((document.getElementById('porsche-button').checked) && (document.getElementById('season1-button').checked) && ((document.getElementById('round9-button').checked) || (document.getElementById('round10-button').checked))) {
-        document.getElementById('round1-button').checked = true;
-    }
-    
+    }  
 
     let roundHtml;
 
@@ -62,7 +41,6 @@ function roundToggleStandings() {
         roundHtml = "/standings/championshipcircuit/season1/round8.html";
     }
     //CHAMPIONSHIP SEASON 2
-    /*
     if ((document.getElementById('championship-button').checked) && (document.getElementById('season2-button').checked) && (document.getElementById('round1-button').checked)) {
         roundHtml = "/standings/championshipcircuit/season2/round1.html";
     }
@@ -87,13 +65,6 @@ function roundToggleStandings() {
     if ((document.getElementById('championship-button').checked) && (document.getElementById('season2-button').checked) && (document.getElementById('round8-button').checked)) {
         roundHtml = "/standings/championshipcircuit/season2/round8.html";
     }
-    if ((document.getElementById('championship-button').checked) && (document.getElementById('season2-button').checked) && (document.getElementById('round9-button').checked)) {
-        roundHtml = "/standings/championshipcircuit/season2/round9.html";
-    }
-    if ((document.getElementById('championship-button').checked) && (document.getElementById('season2-button').checked) && (document.getElementById('round10-button').checked)) {
-        roundHtml = "/standings/championshipcircuit/season2/round10.html";
-    }
-    */
     //PORSCHE SEASON 1
     if ((document.getElementById('porsche-button').checked) && (document.getElementById('season1-button').checked) && (document.getElementById('round1-button').checked)) {
         roundHtml = "/standings/porschecup/season1/round1.html";
@@ -137,32 +108,14 @@ function roundToggleStandings() {
 
 
 function roundToggleResults() {
-    if ((document.getElementById('championship-button').checked) && (document.getElementById('season1-button').checked)) {  //if championship circuit and season 1 is selected, hide round 9 and 10
+    if ((document.getElementById('championship-button').checked)) {  //if championship circuit is selected, show season 2
         makeDefault();
-        document.getElementById('rnd9').style.display = 'none';
-        document.getElementById('rnd10').style.display = 'none';
-    }
-    /*
-    if ((document.getElementById('championship-button').checked) && (document.getElementById('season2-button').checked)) {  //if championship circuit and season 2 is selected, show round 9 and 10
-        makeDefault();
-        document.getElementById('rnd9').style.display = 'block';
-        document.getElementById('rnd10').style.display = 'block';
     }
     
-    if (document.getElementById('porsche-button').checked) {    //if porsche cup is selected, hide season 2, round 9, round 10 and check season 1
+    if (document.getElementById('porsche-button').checked) {    //if porsche cup is selected, hide season 2 and check season 1
         makeDefault();
         document.getElementById('s2btn').style.display = 'none';
         document.getElementById('season1-button').checked = true;
-        document.getElementById('rnd9').style.display = 'none';
-        document.getElementById('rnd10').style.display = 'none';
-    }
-    */
-    //if championship circuit, season 1 and round 9 or round 10 are selected, and you switch to championship circuit season 1 or porsche cup it will default to round 1
-    if ((document.getElementById('championship-button').checked) && (document.getElementById('season1-button').checked) && ((document.getElementById('round9-button').checked) || (document.getElementById('round10-button').checked))) {
-        document.getElementById('round1-button').checked = true;
-    }
-    if ((document.getElementById('porsche-button').checked) && (document.getElementById('season1-button').checked) && ((document.getElementById('round9-button').checked) || (document.getElementById('round10-button').checked))) {
-        document.getElementById('round1-button').checked = true;
     }
     
 
@@ -194,7 +147,6 @@ function roundToggleResults() {
         roundHtml = "/results/championshipcircuit/season1/round8.html";
     }
     //CHAMPIONSHIP SEASON 2
-    /*
     if ((document.getElementById('championship-button').checked) && (document.getElementById('season2-button').checked) && (document.getElementById('round1-button').checked)) {
         roundHtml = "/results/championshipcircuit/season2/round1.html";
     }
@@ -219,13 +171,6 @@ function roundToggleResults() {
     if ((document.getElementById('championship-button').checked) && (document.getElementById('season2-button').checked) && (document.getElementById('round8-button').checked)) {
         roundHtml = "/results/championshipcircuit/season2/round8.html";
     }
-    if ((document.getElementById('championship-button').checked) && (document.getElementById('season2-button').checked) && (document.getElementById('round9-button').checked)) {
-        roundHtml = "/results/championshipcircuit/season2/round9.html";
-    }
-    if ((document.getElementById('championship-button').checked) && (document.getElementById('season2-button').checked) && (document.getElementById('round10-button').checked)) {
-        roundHtml = "/results/championshipcircuit/season2/round10.html";
-    }
-    */
     //PORSCHE SEASON 1
     if ((document.getElementById('porsche-button').checked) && (document.getElementById('season1-button').checked) && (document.getElementById('round1-button').checked)) {
         roundHtml = "/results/porschecup/season1/round1.html";
@@ -256,3 +201,46 @@ function roundToggleResults() {
         $('#circuit').load(roundHtml);
     }
 }
+
+
+
+
+/* leaving this here for possible future use (round 9 and round 10 buttons)
+
+function makeDefault() {
+    document.getElementById('s2btn').style.display = 'block';
+
+    document.getElementById('rnd9').style.display = 'block';
+    document.getElementById('rnd10').style.display = 'block';
+}
+
+function roundToggleStandings() {
+    if ((document.getElementById('championship-button').checked) && (document.getElementById('season1-button').checked)) {  //if championship circuit and season 1 is selected, hide round 9 and 10/
+        makeDefault();
+        document.getElementById('rnd9').style.display = 'none';
+        document.getElementById('rnd10').style.display = 'none';
+    }
+    
+    if ((document.getElementById('championship-button').checked) && (document.getElementById('season2-button').checked)) {  //if championship circuit and season 2 is selected, show round 9 and 10
+        makeDefault();
+        document.getElementById('rnd9').style.display = 'block';
+        document.getElementById('rnd10').style.display = 'block';
+    }
+    
+    if (document.getElementById('porsche-button').checked) {    //if porsche cup is selected, hide season 2, round 9, round 10 and check season 1
+        makeDefault();
+        document.getElementById('s2btn').style.display = 'none';
+        document.getElementById('season1-button').checked = true;
+        document.getElementById('rnd9').style.display = 'none';
+        document.getElementById('rnd10').style.display = 'none';
+    }
+    
+
+    //if championship circuit, season 1 and round 9 or round 10 are selected, and you switch to championship circuit season 1 or porsche cup it will default to round 1
+    if ((document.getElementById('championship-button').checked) && (document.getElementById('season1-button').checked) && ((document.getElementById('round9-button').checked) || (document.getElementById('round10-button').checked))) {
+        document.getElementById('round1-button').checked = true;
+    }
+    if ((document.getElementById('porsche-button').checked) && (document.getElementById('season1-button').checked) && ((document.getElementById('round9-button').checked) || (document.getElementById('round10-button').checked))) {
+        document.getElementById('round1-button').checked = true;
+    }
+*/
