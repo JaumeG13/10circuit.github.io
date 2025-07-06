23c4883a894fad9ff9e755558a081e69197b4c13
JaumeG13 <119821051+JaumeG13@users.noreply.github.com>
2025-07-06 10:11:00 +0200

    formatted date and race time columns

--- assets/js/results.js
+++ assets/js/results.js
@@ -20,12 +20,44 @@ document.addEventListener("DOMContentLoaded", () => {
       const races = doc.data();
       const item = document.createElement("li");
       item.classList.add("result");
+
+      //date to readable format
+      const rawDate = races.Date;
+      const date = new Date(rawDate);
+
+      const options = { day: '2-digit', month: 'short', year: 'numeric' };
+      const formattedDate = date.toLocaleDateString('en-GB', options);
+      //end of readable date
+      
+      //time ms to hh:mm:ss.xxx
+      function formatTime(ms) {
+        const hours = Math.floor(ms / 3600000);
+        const minutes = Math.floor((ms % 3600000) / 60000);
+        const seconds = Math.floor((ms % 60000) / 1000);
+        const milliseconds = ms % 1000;
+    
+        const h = String(hours).padStart(1, '0');
+        const m = String(minutes).padStart(2, '0');
+        const s = String(seconds).padStart(2, '0');
+        const msStr = String(milliseconds).padStart(3, '0');
+    
+        // If hours are zero, omit them
+        if (hours === 0) {
+            return `${m}:${s}.${msStr}`;
+        } else {
+            return `${h}:${m}:${s}.${msStr}`;
+        }
+      }
+    
+      const formatted = formatTime(races.Result[0].TotalTime);
+      //end of time calculation
+
       item.innerHTML = `
-        <span>${races.Date}</span>
+        <span>${formattedDate}</span>
         <span>${races.name}</span>
         <span>${races.Result[0].DriverName}</span>
         <span>${races["document-id"]}</span>
-        <span>${races.Result[0].TotalTime}</span>
+        <span>${formatted}</span>
       `;
       list.appendChild(item);
     });
